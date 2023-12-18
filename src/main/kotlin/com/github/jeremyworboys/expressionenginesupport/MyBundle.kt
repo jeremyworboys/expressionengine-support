14c3c88dbfe9adc4f698ff96106a07fdfd957f23
GitHub Action <action@github.com>
2023-12-18 20:51:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/jeremyworboys/expressionenginesupport/MyBundle.kt
+++ src/main/kotlin/com/github/jeremyworboys/expressionenginesupport/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jeremyworboys.expressionenginesupport
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jeremyworboys/expressionenginesupport/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/jeremyworboys/expressionenginesupport/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jeremyworboys.expressionenginesupport.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
